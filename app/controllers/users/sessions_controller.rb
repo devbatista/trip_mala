79cba982c831dbf91a3b2360c01af8416a540ae2
Rafael Batista <rafael@devbatista.com>
2025-05-19 18:42:24 -0300

    criando controller de sessão que herda do devise session_controller

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,17 @@
+class User::SessionsController < Devise::SessionsController
+  respond_to :json
+
+  private
+
+    def respond_with(resource, _opts = {})
+      render json: {message: 'Login successful', user: resource}, status: :ok
+    end
+
+    def respond_to_destroy
+      if current_user
+        render json: {message: 'Logout successful'}, status: :ok
+      else
+        render json: {message: 'User not logged in'}, status: :unauthorized
+      end
+    end
+end
