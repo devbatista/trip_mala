6a4acbd007b6fa962365db7def3be66ac0e167c0
Rafael Batista <rafael@devbatista.com>
2025-05-19 20:39:04 -0300

    criando controller de api para checklists

---
+++ app/controllers/api/trip_checklists_controller.rb
@@ -0,0 +1,44 @@
+class Api::TripChecklistsController < ApplicationController
+  before_action :authentication_user!
+  before_action :set_trip_checklist, except: :create
+
+  def index
+    @trip_checklists = current_user.trip_checklists
+    render json: @trip_checklists, status: :ok
+  end
+
+  def show
+    render json: @trip_checklist, include: :checklist_items, status: :ok
+  end
+
+  def create
+    @trip_checklist = current_user.trip_checklist.new(trip_checklist_params)
+    if @trip_checklist.save
+      render json: @trip_checklist, status: :created
+    else
+      render json: {errors: @trip_checklist.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @trip_checklist.update(trip_checklist_params)
+      render json: @trip_checklist, status: :ok
+    else
+      render json: {errors: @trip_checklist.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @trip_checklist.destroy
+    head :no_content
+  end
+
+  private
+    def set_trip_checklist
+      @trip_checklist = current_user.trip_checklist.find(params[:id])
+    end
+
+    def trip_checklist_params
+      params.require(:trip_checklist).permit(:name)
+    end
+end
