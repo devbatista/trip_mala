56de64be70887ba1a7a827868dca5cd07a9985fc
Rafael Batista <rafael@devbatista.com>
2025-05-20 11:13:04 -0300

    corrigindo relacionamento na model ChecklistItem

--- app/models/checklist_item.rb
+++ app/models/checklist_item.rb
@@ -1,6 +1,6 @@
 class ChecklistItem < ApplicationRecord
   belongs_to :trip_checklist
-  belongs_to :categories, optional: true
+  belongs_to :category, optional: true
 
   validates :name, presence: true
   validates :checked, inclusion: {in: [true, false]}
