3646560e8ef7d535a47da7e3fc72600c8c19a84b
Rafael Batista <rafael@devbatista.com>
2025-05-21 20:54:15 -0300

    ajuste de testes

--- spec/models/trip_checklist_spec.rb
+++ spec/models/trip_checklist_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe TripChecklist, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:user) }
+  it { should have_many(:checklist_items).dependent(:destroy) }
+
+  it 'is valid with name and username' do
+    checklist = build(:trip_checklist)
+    expect(checklist).to(be_valid)
+  end
+
+  it 'is invalid without name' do
+    checklist = build(:trip_checklist, name: nil)
+    expect(checklist).to_not(be_valid)
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   it 'is valid with valid email and password' do
     user = build(:user)
-    expect(user).to be_valid
+    expect(user).to(be_valid)
   end
 
   it 'is invalid without email' do
     user = build(:user, email: nil)
-    expect(user).to_not be_valid
+    expect(user).to_not(be_valid)
   end
 end
