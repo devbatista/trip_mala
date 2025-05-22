565595c2f0461dc5e306d35994c1f537f988bea5
Rafael Batista <rafael@devbatista.com>
2025-05-21 21:02:17 -0300

    criando regra da model trip_list

--- app/models/trip_list.rb
+++ app/models/trip_list.rb
@@ -1,3 +1,14 @@
 class TripList < ApplicationRecord
   belongs_to :user
+
+  validates :title, :destination, :departure_date, :return_date, presence: true
+  validate :return_after_departure
+
+  private
+  
+    def return_after_departure
+      if return_date && departure_date && return_date < departure_date
+        errors.add(:return_date, "must be after the departure date")
+      end
+    end
 end
