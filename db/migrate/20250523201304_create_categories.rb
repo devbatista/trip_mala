91552f3891d948917ceb848e26e27087246b5838
Rafael Batista <rafael@devbatista.com>
2025-05-23 17:13:27 -0300

    recriando migration Categories

---
+++ db/migrate/20250523201304_create_categories.rb
@@ -0,0 +1,11 @@
+class CreateCategories < ActiveRecord::Migration[7.1]
+  def change
+    create_table :categories do |t|
+      t.string :name
+      t.references :trip_checklist, null: false, foreign_key: true
+      t.timestamps
+    end
+    
+    add_index :categories, :trip_checklist_id
+  end
+end
