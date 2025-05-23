5fead36a88bc242ad79c2c2f492917077c3af0e1
Rafael Batista <rafael@devbatista.com>
2025-05-23 17:14:22 -0300

    ajustando migrations

---
+++ db/migrate/20250523201408_create_shared_lists.rb
@@ -0,0 +1,11 @@
+class CreateSharedLists < ActiveRecord::Migration[7.1]
+  def change
+    create_table :shared_lists do |t|
+      t.references :trip, null: false, foreign_key: true
+      t.string :email
+      t.string :token
+      t.timestamps
+    end
+    add_index :shared_lists, :trip_id
+  end
+end
