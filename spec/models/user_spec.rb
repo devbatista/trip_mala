0ce951b79c7256dbdc34c9a282ef5351e4c2ff05
Rafael Batista <rafael@devbatista.com>
2025-05-26 13:27:35 -0300

    colocando teste de validação de senha

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,6 +6,9 @@ RSpec.describe User, type: :model do
   it { should allow_value('teste@email.com').for(:email) }
   it { should_not allow_value('email_invalido').for(:email) }
 
+  it { should validate_presence_of(:password).on(:create) }
+  it { should validate_length_of(:password).is_at_least(6).on(:create) }
+
   it 'is valid with valid email and password' do
     user = build(:user)
     expect(user).to(be_valid)
